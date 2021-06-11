0a6a9abb1f23d562517fd74d7fefc721227452d6
Mark Anderson <markanderson8162@gmail.com>
2021-06-11 14:28:05 -0500

    created number guessing game

---
+++ selectionStatementExercise/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace selectionStatementExercise
+{
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+			var r = new Random();
+			var favNumber = r.Next(1, 1000);
+			
+			Console.WriteLine("What is my favorite number?");
+			var userInput = int.Parse(Console.ReadLine());
+
+			while(userInput != favNumber)
+			{
+				if (userInput > favNumber)
+				{
+					Console.WriteLine("That's too high! Try again!");
+					userInput = int.Parse(Console.ReadLine());
+
+				}
+				else if (userInput < favNumber)
+				{
+					Console.WriteLine("That's too low! Try again!");
+					userInput = int.Parse(Console.ReadLine());
+
+				}
+				else
+				{
+					Console.WriteLine("That's correct!");
+
+				}
+			}
+
+			//if(userInput > favNumber)
+			//{
+			//	Console.WriteLine("That's too high! Try again!");
+
+
+			//}
+			//else if(userInput < favNumber)
+			//{
+			//	Console.WriteLine("That's too low! Try again!");
+
+			//}
+			//else
+			//{
+			//	Console.WriteLine("Nevermind");
+			//}
+		}
+	}
+}
